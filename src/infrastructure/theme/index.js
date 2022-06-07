eebb7b1bce43b36227acdaf82f73ad791dae6adf
pagar22 <aaryanpagar@hotmail.com>
2022-06-07 12:39:52 +0200

    Created Custom Theme

---
+++ src/infrastructure/theme/index.js
@@ -0,0 +1,14 @@
+import { colors } from './colors';
+import { fonts, fontWeight, fontSizes } from './fonts';
+import { sizes } from './sizes';
+import { space, lineHeight } from './spacing';
+
+export const theme = {
+    colors,
+    fonts,
+    fontWeight,
+    fontSize,
+    sizes,
+    space,
+    lineHeight,
+}
